92a600aaa9fd691125c357b924c0593133d7f214
Gloterianota349 <guilhermemascarenhas2708@gmail.com>
2025-11-24 22:38:05 -0300

    fix: Mudança de DB

--- Config/configuration.php
+++ Config/configuration.php
@@ -1,9 +1,9 @@
 <?php
 
-define("DB_NAME", "agrybem");
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", '#Guisil2017');
+define("DB_NAME", "if0_40502604_agrybem");
+define("DB_HOST", "sql102.infinityfree.com");
+define("DB_USER", "if0_40502604");
+define("DB_PASSWORD", 'vBpg3Kkj998');
 define("DB_PORT", "3306");
 
 ?>
